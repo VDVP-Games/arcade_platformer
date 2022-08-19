0a243c25e5a6213850dc087e2d1b34d2c4c833a6
Salatiktik <muz.ugol@gmail.com>
2022-08-19 20:19:03 +0300

    start equipment

--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -4,10 +4,10 @@ using System.Timers;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour
+public class movement : MonoBehaviour
 {
     Rigidbody2D rb;
-    //Animator anim;
+    Animator anim;
 
     int dash_cd = 0;
     int direction = 1;
@@ -15,7 +15,7 @@ public class move : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        //anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,42 +24,41 @@ public class move : MonoBehaviour
         float Axis = Input.GetAxis("Horizontal");
         if (Axis < 0 && dash_cd == 0)
         {
-            //anim.SetBool("running", true);
+            anim.SetBool("running", true);
             transform.localScale = new Vector3(-1, 1, 1);
             rb.velocity = new Vector2(Axis * 7, rb.velocity.y);
             direction = -1;
         }
         else if (Axis > 0 && dash_cd == 0)
         {
-            //anim.SetBool("running", true);
+            anim.SetBool("running", true);
             transform.localScale = new Vector3(1, 1, 1);
             rb.velocity = new Vector2(Axis * 7, rb.velocity.y);
             direction = 1;
         }
         else
         {
-            //anim.SetBool("running", false);
+            anim.SetBool("running", false);
         }
 
         if (Input.GetKeyDown("space") && Math.Abs(rb.velocity.y) < 0.001)
         {
-            //anim.SetBool("jumping", true);
+            anim.SetBool("jumping", true);
             rb.velocity = new Vector3(rb.velocity.x, 15, 0);
         }
         if (rb.velocity.y < 0.001)
         {
-            //anim.SetBool("jumping", false);
-            //anim.SetBool("fall", true);
+            anim.SetBool("jumping", false);
+            anim.SetBool("fall", true);
         }
         if (Math.Abs(rb.velocity.y) < 0.001)
         {
-            //anim.SetBool("jumping", false);
-            // anim.SetBool("fall", false);
+            anim.SetBool("jumping", false);
+            anim.SetBool("fall", false);
         }
 
         if (Input.GetKeyDown("q") && dash_cd == 0)
         {
-            //anim.SetBool("dash", true);
             dash_cd = 200;
             Debug.Log(direction);
             rb.velocity = new Vector3(direction * 20, rb.velocity.y, 0);
@@ -69,10 +68,6 @@ public class move : MonoBehaviour
         {
             dash_cd--;
             Debug.Log(dash_cd);
-        }
-        else
-        {
-            //anim.SetBool("dash", false);
         }
     }
 }
---
+++ Assets/scripts/traps.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class traps : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
