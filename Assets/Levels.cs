82579eb82a41e9a8215b5dfbccdf4a2ea82293f0
Аrtem Sorokin 153503 <93012013+Axel7509@users.noreply.github.com>
2022-08-22 00:43:56 +0300

    //menu

---
+++ Assets/Levels.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Levels : MonoBehaviour
+{
+    public void Level_1()
+    {
+        SceneManager.LoadScene(1);// загружать сцену по индексу
+    }
+
+}
