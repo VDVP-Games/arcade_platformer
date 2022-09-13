330503d5afda3294178163c95d5ec2e6f04f1db9
Аrtem Sorokin 153503 <93012013+Axel7509@users.noreply.github.com>
2022-09-14 00:19:26 +0300

    tipo_end

--- Assets/Scenes/menu/End_Menu.cs
+++ Assets/Scenes/menu/End_Menu.cs
@@ -7,12 +7,13 @@ using UnityEngine.UI;
 public class End_Menu : MonoBehaviour
 {
     public Text coinsText;
-    public Behaviour Inventory_Canvas;
+    [SerializeField] private GameObject Inventory_Canvas;
 
 
     void Start()
     {
-        Inventory_Canvas.enabled = !Inventory_Canvas.enabled;
+        Debug.Log(Inventory_Canvas);
+        Inventory_Canvas.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -21,34 +22,34 @@ public class End_Menu : MonoBehaviour
         {
             Time.timeScale = 0f;
             Debug.Log("end");
-           // GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            // GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
 
             int coins = PlayerPrefs.GetInt("c1") + PlayerPrefs.GetInt("c2") + PlayerPrefs.GetInt("c3");
-            
+
             int lcoins = SceneManager.GetActiveScene().buildIndex;
             string str = "coins" + lcoins.ToString();
             lcoins = PlayerPrefs.GetInt(str);
             Debug.Log(coins);
 
-            
+
             if (coins > lcoins)
             {
-                PlayerPrefs.SetInt("coins", PlayerPrefs.GetInt("coins") + coins - lcoins);  
+                PlayerPrefs.SetInt("coins", PlayerPrefs.GetInt("coins") + coins - lcoins);
                 lcoins = coins;
             }
             Debug.Log("all coins");
             Debug.Log(PlayerPrefs.GetInt("coins"));
-            PlayerPrefs.SetInt(str,lcoins);
+            PlayerPrefs.SetInt(str, lcoins);
 
             coinsText.text = "Collected coins " + lcoins.ToString() + " / 3";
-            Inventory_Canvas.enabled = !Inventory_Canvas.enabled;
+            Inventory_Canvas.SetActive(true);
 
             PlayerPrefs.SetInt("time_coins", 0);
             PlayerPrefs.SetInt("c1", 0);
             PlayerPrefs.SetInt("c2", 0);
             PlayerPrefs.SetInt("c3", 0);
             Debug.Log(PlayerPrefs.GetInt(str));
-            
+
         }
     }
 }
